942e3b5ac80434a3c28c6b0317a21436ec3cef8a
gdeparis <smartcoder@pc116.home>
2016-07-08 14:13:52 +0200

    init project
    init project

---
+++ app/src/main/java/com/kenfestoche/smartcoder/kenfestoche/webservices/WebService.java
@@ -0,0 +1,83 @@
+package com.kenfestoche.smartcoder.kenfestoche.webservices;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by smartcoder on 07/07/16.
+ */
+public class WebService {
+
+    public static final String URL_ZENAPP = "http://www.smartcoder-dev.fr/ZENAPP/webservice/";
+
+    public JSONArray Connect(String Login, String Pass) {
+        StringBuilder builder = new StringBuilder();
+        JSONArray Messages = null;
+    try{
+        URL url = new URL(URL_ZENAPP+"WS_Login.php");
+        HttpURLConnection client = null;
+        try {
+            client = (HttpURLConnection) url.openConnection();
+            client.setRequestMethod("POST");
+            //client.setRequestProperty("login",Login);
+            //client.setRequestProperty("password",Pass);
+            //client.setDoOutput(true);
+            //client.connect();
+
+            String urlParameters = "login="+Login+"&cn=&password="+Pass;
+
+            // Send post request
+            client.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(client.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.flush();
+            wr.close();
+            int status = client.getResponseCode();
+
+            switch (status) {
+                case 200:
+                case 201:
+                    BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                    //StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        builder.append(line+"\n");
+                    }
+                    //builder.close();
+
+            }
+            //OutputStream outputPost = new BufferedOutputStream(client.getOutputStream());
+            Messages = new JSONArray(builder.toString());
+
+        } catch (IOException e){
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    } catch (MalformedURLException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+        return Messages;
+    }
+
+}
